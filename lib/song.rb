42ced8bbd0b172f680354b79e2b213909da8c058
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-09-08 02:15:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,9 +2,13 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def initialize(name)
+    @name = name
+    @@all << self
+
   def self.all
     @@all
-  end
+  end 
 
   def save
     self.class.all << self
